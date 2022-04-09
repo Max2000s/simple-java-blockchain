dd9ce9ae9ed351e8627aff71f1f5b3dd2bdc76ff
Max2000s <80688802+Max2000s@users.noreply.github.com>
2022-04-09 20:03:25 +0200

    initial push with code

---
+++ src/MainClass.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class MainClass {
+    public static void main(String[] args) {
+        List<Block> blockchain = new ArrayList<>();
+        int prefix = 4;
+        String prefixString = new String(new char[prefix]).replace('\0', '0');
+    }
+
+    @Test
+    public void givenBlockchain_whenNewBlockAdded_thenSuccess() {
+
+    }
+}
+
