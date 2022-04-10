555e702baff0ae9d77cc6eccf3881dba99b571e7
Max2000s <80688802+Max2000s@users.noreply.github.com>
2022-04-10 19:56:27 +0200

    created methods for BlockChain Class

--- src/BlockChain.java
+++ src/BlockChain.java
@@ -1,3 +1,19 @@
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BlockChain {
-    Block[] blockchain;
+    List<Block> blockchain = new ArrayList<Block>();
+
+    private void addGenesisBlock() throws NoSuchAlgorithmException {
+        Block genesisBlock = new Block(
+                "",
+                "0000000000000000000000000000000000000000000000000000000000000000",
+                123);
+        blockchain.add(genesisBlock);
+    }
+
+    private void addBlockToChain(Block currentBlock) {
+        blockchain.add(currentBlock);
+    }
 }
